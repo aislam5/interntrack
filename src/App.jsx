0a1fe496dad905636365ee1bea3e4a66440bfb3e
Abdullah <abdullahislam900@gmail.com>
2026-03-12 20:34:21 -0400

    added logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,37 @@ import {useState, useEffect} from 'react'
 import {supabase} from './supabaseClient'
 import Auth from './pages/Auth'
 
+
+
 function App() {
-const [session, setSession] = useState(null)
+  const [session, setSession] = useState(null)
 
-useEffect(() => {
-  supabase.auth.getSession().then(({data: {session}}) => {
-    setSession(session)
+  async function handleLogout() {
+    await supabase.auth.signOut()
+  }
 
-  })
+  useEffect(() => {
+    supabase.auth.getSession().then(({data: {session}}) => {
+      setSession(session)
 
-  supabase.auth.onAuthStateChange((_event, session) => {
-    setSession(session)
-  })
-}, [])
+    })
 
-return (
-  <div>
-    {session ? <h1>You are logged in</h1> : <Auth supabase={supabase} />}
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+
+  return (
+    <div>
+      {session ? (
+      <div>
+        <h1>You are logged in</h1>
+        <button onClick={handleLogout}>Log Out</button>
+      </div>
+    ) : <Auth supabase={supabase} />}
   </div>
-)
+  )
 }
 
 export default App
